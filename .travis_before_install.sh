c35cbd9b70c4f46a9004b3418e1a2b1883dd0072
Yang Wu <wuyang@drwu.ga>
2020-08-07 00:53:30 +0800

    rm -rf ./util/support_scripts/tests

--- .travis_before_install.sh
+++ .travis_before_install.sh
@@ -23,7 +23,8 @@ case $TRAVIS_OS_NAME in
 	    wget -qO /tmp/trinity.tar.gz https://github.com/trinityrnaseq/trinityrnaseq/archive/Trinity-v${TRINITY}.tar.gz
 	    tar -zxvf /tmp/trinity.tar.gz && cd trinityrnaseq-Trinity-v${TRINITY}
 	    export PATH=/usr/local/bin:$PATH
-	    make CXX=g++ CC=gcc && make plugins CXX=g++ CC=gcc && cd ..
+	    make CXX=g++ CC=gcc && make plugins CXX=g++ CC=gcc
+	    rm -rf ./util/support_scripts/tests && cd ..  # It is not compatible with python 3 and pytest!
 	    # The version of sra-tools in macOS channel is not the latest, so don't specify it here.
 	    # The default version of samtools is 1.4, which doesn't work, so set it to the latest (1.10).
 	    conda install -qy python=$PYTHON sra-tools samtools=1.10 numpy bowtie bowtie2 kmer-jellyfish salmon trimmomatic
